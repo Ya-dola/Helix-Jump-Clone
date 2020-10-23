cf0400b3c8be653e25f6f5c63ca4a39fee74e4c8
Ya-dola <Ya-dola@users.noreply.github.com>
2020-10-23 18:28:41 +0300

    Added Camera Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerBallController cameraTarget;
+    // Keeps the starting distance between the camera and the player ball
+    private float offset;
+
+    private void Awake()
+    {
+        offset = transform.position.y - cameraTarget.transform.position.y;
+    }
+
+    void Update()
+    {
+        // Moves the camera smoothly to targets' height - on Y Axis
+        Vector3 currentPos = transform.position;
+        currentPos.y = cameraTarget.transform.position.y + offset;
+        transform.position = currentPos;
+    }
+}
